35d74fc8c45711dc38b4e3c8ef5ca91a0616ee36
SrinivasuluT <sri10586@gmail.com>
2014-03-27 14:50:23 +0530

    ongole

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Website::Application.configure do
   # config.assets.manifest = YOUR_PATH
   #RAILS_ENV=production bundle exec rake assets:precompile
   config.assets.compile = true
-  config.assets.initialize_on_precompile = false
+  #config.assets.initialize_on_precompile = false
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
