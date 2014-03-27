43f0b097132dbccd4b65af0266ea6650d392d5b4
SrinivasuluT <sri10586@gmail.com>
2014-03-27 14:34:55 +0530

    ongole

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Website::Application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
-  #config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = false
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
