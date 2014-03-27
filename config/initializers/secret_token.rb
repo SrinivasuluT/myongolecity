b6d7149732cb7d5b0e5cb6ed1de19c1ed84431b9
SrinivasuluT <sri10586@gmail.com>
2014-03-27 09:49:17 +0530

    ongole

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '2f8116f38a4b5e038e1003f3779ba0ef9529c86e5d8803edacf0ecc1fd940f19ebb582bfcfccff7e3cd7788f9a677212967f5f20fc399f6a68f8e127f0281eee'
