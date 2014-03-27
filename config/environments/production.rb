ae45ff43beef049815aea50fdddee6f03cba4b8c
SrinivasuluT <sri10586@gmail.com>
2014-03-27 22:12:55 +0530

    ongole

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,9 @@ Website::Application.configure do
   # config.assets.manifest = YOUR_PATH
   #RAILS_ENV=production bundle exec rake assets:precompile
   config.assets.compile = true
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif application-print.css)
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.initialize_on_precompile = true
+  config.serve_static_assets = true
   #config.assets.initialize_on_precompile = false
 
   # Specifies the header that your server uses for sending files
